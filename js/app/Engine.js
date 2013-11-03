9453538dc6f0ed6d725cea4abdb8b99002ca1dff
Anthony Deaver <lordbishop@gmail.com>
2013-11-03 01:32:09 -0400

    phase one of refactor
    Engine: 80%
    Layout: 50%
    User: 10%

--- js/app/Engine.js
+++ js/app/Engine.js
@@ -1,4 +1,130 @@
-var Engine = function(config, data) {
+var Engine = (function() {
+    'use strict';
+    var DEBUG = true;
+
+    function Engine(app) {
+        var _defaults = {
+                vars : {
+                    squirrel: { value : 'nutPicker', muteable: true},
+                    coconut: { value : 'treenut', muteable: false}
+                }
+            },
+            self = {};
+
+        this.initialize.apply(this.arguments);
+    }
+
+    // getter / setter for debug
+    Object.defineProperty(Engine, "debug", {
+        set: function (x) {
+            // document.write("in property set accessor" + newLine);
+            DEBUG = x;
+        },
+        get: function () {
+            // document.write("in property get accessor" + newLine);
+            return DEBUG;
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Engine.log = function(str) {
+        if (DEBUG) {
+            $( "#log .inner" ).prepend( $('<div>').addClass('logEntry').text(str) );
+       } else {
+            console.log(str);
+        }
+    };
+
+    // Gets a guid
+    Engine.GUID = function() {
+        return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    };
+
+    Engine.toggleLog = function() {
+        console.log('toggle');
+        $('#log').toggle();
+    };
+
+    Engine.init = function() {
+        // Not in debug mode, remove the log
+        if(!DEBUG) {
+            var ele = document.getElementById("log");
+            ele.parentNode.removeChild(ele);
+        }
+
+        if (!User) {
+            throw new Error('Unable to locate User class');
+        }
+
+        $('.close').on('click', function() {
+            $(this).closest('.panel').hide();
+        });
+
+        Layout.init();
+
+
+    };
+
+    Engine.prototype.initialize = function() {
+        // Not in debug mode, remove the log
+        if(!DEBUG) {
+            var ele = document.getElementById("log");
+            ele.parentNode.removeChild(ele);
+        }
+
+        if (!User) {
+            throw new Error('Unable to locate User class');
+        }
+        Layout.init();
+    };
+
+    // Getters and Setters
+    Engine.prototype.getConfig = function(attr) {
+        return _defaults[config[attr]];
+    };
+
+    // Generic: Values must exist in _defaults.vars and be muteable
+    Engine.prototype.get = function(str) {
+        str = str.toLowerCase();
+        if (_defaults.vars[str] !== undefined) { return _defaults.vars[str].value; }
+        return false;
+    };
+
+    Engine.prototype.set = function(attr, val) {
+        attr = attr.toLowerCase();
+        if (_defaults.vars[attr] !== undefined && _defaults.vars[attr].muteable) {
+            _defaults.vars[attr].value = val;
+            return true;
+        }
+        return false;
+    };
+
+    // Utility functions
+    Engine.loadJSON = function(path, callback) {
+        var httpRequest = new XMLHttpRequest(),
+            that = this;
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if(DEBUG) that.log('data set loaded');
+                    if (callback) { callback(data); }
+                }
+            }
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+    };
+
+    return Engine;
+
+})();
+
+var Engine2 = function(config, data) {
     'use strict';
     var DEBUG = true,
         _defaults = {
