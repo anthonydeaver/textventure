0983bb65deb4cc5574ad92ad35749d1718275cfa
Anthony Deaver <lordbishop@gmail.com>
2013-11-07 15:00:10 -0500

    small changes
    checks if a room has a monster and attempts to 'roll' for one.

--- js/app/Rooms.js
+++ js/app/Rooms.js
@@ -1,26 +1,27 @@
 var Rooms = (function(parent) {
     'use strict';
     var _default = '',
-        _rooms;
+        _rooms,
+        Rooms = {};
 
     function parseRooms(json) {
         _rooms = json.rooms;
-        _default = _rooms.default;
+        _default = _rooms.start;
         Rooms.loadRoom(_default);
     }
-
-    var Rooms = {};
 
     Rooms.initialize = function() {
         Engine.loadJSON('js/app/resources/rooms.json', parseRooms);
     };
 
     Rooms.loadRoom = function(rm) {
-    	$('.locator #exits .button').remove();
-        var room = _rooms[rm];
+        $('.locator #exits .button').remove();
+        var room = _rooms[rm],
+            x,
+            getMonster;
         Engine.setLocation(room);
-        $( "#log .inner" ).prepend( $('<div>').addClass('roomEntry').text(room.desc) );
-        for (var x = 0; x < room.exits.length; x++) {
+        $("#log .inner").prepend($('<div>').addClass('roomEntry').text(room.desc));
+        for (x = 0; x < room.exits.length; x++) {
             var exit = _rooms[room.exits[x]];
             var btn = $('<div>')
                 .addClass('button')
@@ -36,8 +37,11 @@ var Rooms = (function(parent) {
         }
 
         if (room.hasMonster) {
-        	var monsterGen = Math.floor(Math.random() * (100 + 1));
-        	// if()
+            console.log('possible monster');
+        	var getMonster = Math.floor(Math.random() * (100 + 1));
+        	if( getMonster <= (room.hasMonster * 100)) {
+                console.log('we have a monster!');
+            }
         }
     };
 
